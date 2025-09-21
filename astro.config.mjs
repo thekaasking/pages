7f1780ab0ba54738bfe3464a76ea8c6931db8cd8
Razo van Berkel <razovanberkel@gmail.com>
2025-09-21 03:46:49 +0200

    Update GitHub Actions workflow for deployment and adjust Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-  site: 'https://razovanberkel.nl', // set this for sitemap/RSS & GH Pages
+  site: 'https://thekaasking.github.io',
+  base: '/pages',
   output: 'static',
   integrations: [mdx(), sitemap()],
 });
